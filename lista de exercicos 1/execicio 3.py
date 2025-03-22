33feb2c1ceb17a2d6876a7d36cfada88ab28419a
NRovedaM <nicolasrovedamelo@gmail.com>
2025-03-21 22:43:02 -0400

    Add files via upload

---
+++ lista de exercicos 1/execicio 3.py
@@ -0,0 +1,6 @@
+N = int(input("insira um valor inteiro:"))
+
+N_resultado = N - 1
+N_resultadoMenor = N + 1
+
+print(f"seu antecessor é {N_resultado} e seu sucessor é {N_resultadoMenor}")
---
+++ lista de exercicos 1/exercicio 7.py
@@ -0,0 +1,5 @@
+desconto = float(input("valor a calcular desconto: "))
+
+descontado = desconto - (desconto * 0.05)
+
+print(f" o valor a pagar com desconto é de {descontado:.2f}")
