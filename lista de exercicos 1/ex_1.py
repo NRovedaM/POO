6847e0569f2d692a8863dc6b05c89350957c875f
NRovedaM <nicolasrovedamelo@gmail.com>
2025-03-25 21:45:02 -0400

    Delete lista de exercicos 1/ex_1.py

--- lista de exercicos 1/ex_1.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("insira um numero: "))
-numero2 = int(input("insira um segundo numero que vai somar ao primeiro: "))
-
-resultado = numero + numero2
-
-print("o resultado é: ", resultado)
