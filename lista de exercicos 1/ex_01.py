162291e4437abfe913676c5ce5a160f68ef80be3
NRovedaM <nicolasrovedamelo@gmail.com>
2025-03-25 21:50:34 -0400

    Add files via upload

---
+++ lista de exercicos 1/ex_01.py
@@ -0,0 +1,6 @@
+numero = int(input("insira um numero: "))
+numero2 = int(input("insira um segundo numero que vai somar ao primeiro: "))
+
+resultado = numero + numero2
+
+print("o resultado é: ", resultado)
